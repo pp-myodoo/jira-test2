6f7bab7eea226b0305ea854cf7e62b8460cf600e
Patryk Prokurat <pp@myodoo.pl>
2022-07-13 09:54:35 +0200

    updated module

--- pragmatic_helpdesk_jira_connector/models/helpdesk_ticket.py
+++ pragmatic_helpdesk_jira_connector/models/helpdesk_ticket.py
@@ -46,7 +46,7 @@ class HelpdeskTicketInherit(models.Model):
     is_subtask = fields.Boolean(compute=compute_is_epic, store=True)
     is_epic = fields.Boolean(compute=compute_is_epic, store=True)
     show_jira_details = fields.Boolean('Use Ticket For Jira ')
-    jira_tag_ids = fields.Many2many('helpdesk.tag', string='Jira Tags')
+    jira_tag_ids = fields.Many2many('helpdesk.tag', 'helpdesk_jira_tag_helpdesk_ticket_rel', 'helpdesk.tag', 'helpdesk.ticket', string='Jira Tags')
 
     @api.onchange('project_id')
     def onchange_project_id(self):
