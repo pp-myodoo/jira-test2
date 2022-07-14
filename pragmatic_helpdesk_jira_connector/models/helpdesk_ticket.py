54a05607566a12e6c0a6d89f450c2b30605f5fd1
Patryk Prokurat <pp@myodoo.pl>
2022-07-14 14:20:25 +0200

    updated module

--- pragmatic_helpdesk_jira_connector/models/helpdesk_ticket.py
+++ pragmatic_helpdesk_jira_connector/models/helpdesk_ticket.py
@@ -173,14 +173,6 @@ class HelpdeskTicketInherit(models.Model):
                 labels_dict = response['fields']['labels']
 
                 if ticket_id:
-                    tags_ids_list = []
-                    for tag in ticket.tag_ids:
-                        tags_ids_list.append(tag.name)
-                    jira_tags_ids_list = []
-                    for tag in ticket.jira_tag_ids:
-                        jira_tags_ids_list.append(tag.name)
-                    _logger.info(f'TAG IDS 1: {tags_ids_list}')
-                    _logger.info(f'JIRA TAG IDS 1: {jira_tags_ids_list}')
 
                     # remove deleted tags from sync list and ticket tags list
                     for tag in ticket.jira_tag_ids:
@@ -193,11 +185,6 @@ class HelpdeskTicketInherit(models.Model):
                             ticket.tag_ids = [(3, tag.id)]  # unlink
                             ticket.jira_tag_ids = [(3, tag.id)]  # unlink
 
-                    jira_tags_ids_list = []
-                    for tag in ticket.jira_tag_ids:
-                        jira_tags_ids_list.append(tag.name)
-                    _logger.info(f'JIRA TAG IDS 2: {jira_tags_ids_list}')
-
                     # add new tags to sync list
                     for label in labels_dict:
                         helpdesk_tag = self.env['helpdesk.tag'].search([('name', '=', label)])
@@ -207,19 +194,9 @@ class HelpdeskTicketInherit(models.Model):
                             })
                         ticket.jira_tag_ids = [(4, helpdesk_tag.id)]  # link
 
-                    jira_tags_ids_list = []
-                    for tag in ticket.jira_tag_ids:
-                        jira_tags_ids_list.append(tag.name)
-                    _logger.info(f'JIRA TAG IDS 3: {jira_tags_ids_list}')
-
                     # link new sync list to ticket tags list
                     for tag in ticket.jira_tag_ids:
                         ticket.tag_ids = [(4, tag.id)]  # link
-
-                    tags_ids_list = []
-                    for tag in ticket.tag_ids:
-                        tags_ids_list.append(tag.name)
-                    _logger.info(f'TAG IDS 2: {tags_ids_list}')
 
             # ============================= ADDED PART =============================
 
@@ -287,6 +264,17 @@ class HelpdeskTicketInherit(models.Model):
                                 comment_id.jira_id = response.json()['id']
 
                     # ============================= ADDED PART =============================
+                    # SENDING COMMENT AUTHOR ID
+
+                            if comment_id.author_id:
+                                user = self.env['res.users'].search(['partner_id', '=', comment_id.author_id.id])
+                                if user.jira_accountId:
+                                    user_data = {"author": {"accountId": user.jira_accountId}}
+                                    _logger.info(f"COMMENT USER DATA: {data}")
+                                if (user_data and help_tict_id.key) and not comment_id.jira_id:
+                                    response = self.env['res.company'].search([], limit=1).post(
+                                        'issue/' + help_tict_id.key + '/comment', user_data, )
+
                     # SENDING LABELS TO JIRA
                     if help_tict_id.tag_ids:
 
