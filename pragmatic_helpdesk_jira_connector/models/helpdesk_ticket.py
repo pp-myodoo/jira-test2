eab284fee4ed43b8288239ae5e0fb3a2350845c6
Patryk Prokurat <pp@myodoo.pl>
2022-07-14 14:23:39 +0200

    updated module

--- pragmatic_helpdesk_jira_connector/models/helpdesk_ticket.py
+++ pragmatic_helpdesk_jira_connector/models/helpdesk_ticket.py
@@ -269,8 +269,8 @@ class HelpdeskTicketInherit(models.Model):
                             if comment_id.author_id:
                                 user = self.env['res.users'].search(['partner_id', '=', comment_id.author_id.id])
                                 if user.jira_accountId:
-                                    user_data = {"author": {"accountId": user.jira_accountId}}
-                                    _logger.info(f"COMMENT USER DATA: {data}")
+                                    user_data = {"author": {"accountId": str(user.jira_accountId)}}
+                                    _logger.info(f"COMMENT USER DATA: {user_data}")
                                 if (user_data and help_tict_id.key) and not comment_id.jira_id:
                                     response = self.env['res.company'].search([], limit=1).post(
                                         'issue/' + help_tict_id.key + '/comment', user_data, )
